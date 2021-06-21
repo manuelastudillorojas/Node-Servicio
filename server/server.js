66026ad98ed55b7c3a5bd573deeb763969871afe
Manuel <manuel.astudillo.rojass@gmail.com>
2021-06-21 01:13:31 -0400

    RestServer - Usuario

--- server/server.js
+++ server/server.js
@@ -14,17 +14,6 @@ app.use(bodyParser.json())
 app.use(require('./router/usuario'));
 
 
-
-mongoose.connect(process.env.urlDB, {
-    useNewUrlParser: true,
-    //useUnifiedTopology: true,
-    //useFindAndModify: false,
-    useCreateIndex: true
-}, (err, res) => {
-    if (err) throw err;
-    console.log('bd operativo');
-});
-
 app.listen(process.env.PORT, () => {
     console.log('Escuchando puerto: ', process.env.PORT);
 });

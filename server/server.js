db609cbd5b0c9ae89e8ecc7e98ce467639bd5c2c
Manuel <manuel.astudillo.rojass@gmail.com>
2021-06-21 01:15:39 -0400

    RestServer - Usuario

--- server/server.js
+++ server/server.js
@@ -11,7 +11,8 @@ app.use(bodyParser.urlencoded({ extended: false }))
 
 app.use(bodyParser.json())
 
-app.use(require('./router/usuario'));
+
+//app.use(require('./router/usuario'));
 
 
 app.listen(process.env.PORT, () => {

57cc1ed047385729b775642d2658017f47eb694c
Manuel <manuel.astudillo.rojass@gmail.com>
2021-06-21 23:23:30 -0400

    RestServer - Usuario

--- server/config/config.js
+++ server/config/config.js
@@ -22,7 +22,7 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = 'mongodb+srv://Admin:SKJ7qDHhbTH8ttl3@cluster0.a7v8q.mongodb.net/cafe';
+    urlDB = 'mongodb+srv://Admin:SKJ7qDHhbTH8ttl3@cluster0.a7v8q.mongodb.net/cafe/cafe';
 }
 
 process.env.urlDB = urlDB;
